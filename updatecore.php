9fc98883ccaa1100c39533d586e1407b01b54c0b
Mbina <mbinagds@gmail.com>
2025-05-18 12:21:07 +0330

    Update updatecore.php

--- updatecore.php
+++ updatecore.php
@@ -1,5 +1,33 @@
 <?php
-$curl = "https://raw.githubusercontent.com/sshnevis/Server-Screening-Script/refs/heads/master/screening.php";
-$code = file_get_contents($curl);
-eval("?>".$code);
 
+$remote_url = "https://raw.githubusercontent.com/sshnevis/Server-Screening-Script/refs/heads/master/screening.php";
+$local_file = "screening.php";
+
+try {
+    $ch = curl_init($remote_url);
+    if (!$ch) {
+        throw new Exception("curl_init failed");
+    }
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+    $code = curl_exec($ch);
+    $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if ($code === false || $http_status !== 200) {
+        throw new Exception("Download failed");
+    }
+
+    if (file_put_contents($local_file, $code) === false) {
+        throw new Exception("Write failed");
+    }
+
+    echo "Update successful";
+
+} catch (Exception $e) {
+    echo "Update failed";
+}
+?>
